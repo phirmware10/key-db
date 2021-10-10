3fa706469af83cccbbb67a4c9347091f968883f9
phirmware <chibuzor.ojukwu@gmail.com>
2021-10-10 13:13:04 +0100

    feat: added tests for all db methods

--- db.go
+++ db.go
@@ -3,19 +3,21 @@ package keydb
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"strings"
 )
 
 var (
-	DB_FILE = "./logs"
+	DB_FILE    = "./logs"
 	INDEX_FILE = "./index.json"
 )
 
 type (
 	DB struct {
 		f        *os.File
+		indexf   *os.File
 		byteSize int
 	}
 
@@ -25,7 +27,6 @@ type (
 		Location int
 		Max      int
 	}
-
 )
 
 func init() {
@@ -33,46 +34,50 @@ func init() {
 	log.SetPrefix(fmt.Sprintf("%d]: DB: ", pid))
 }
 
-func NewDB() DB {
-	f, err := os.OpenFile(DB_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	indexf, err := os.OpenFile(INDEX_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	defer indexf.Close()
+func NewDB(f *os.File, indexf *os.File) *DB {
+	// f, err := os.OpenFile(DB_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	// indexf, err := os.OpenFile(INDEX_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 
 	info, err := f.Stat()
 
 	if err != nil {
-		log.Fatalf("FATAL: Error opening db file, %s", err)
+		log.Fatalf("FATAL: Error getting file information:, %s", err)
 	}
 
-	hf := DB{
+	hf := &DB{
 		f:        f,
+		indexf:   indexf,
 		byteSize: int(info.Size()),
 	}
 
 	return hf
 }
 
-func (hf DB) ShutDown() {
-	log.Println("Shutting down DB")
-	defer hf.f.Close()
+func (hf *DB) Close() error {
+	if err := hf.indexf.Close(); err != nil {
+		return err
+	}
+	return hf.f.Close()
 }
 
-func getIndexes() HashTable {
-	b, err := os.ReadFile(INDEX_FILE)
+func (hf *DB) getIndexes() HashTable {
+	info := hf.indexf.Name()
+
+	data, err := ioutil.ReadFile(info)
+	if err != nil {
+		log.Fatalf("Error reading indexes: %s", err)
+	}
 	indexes := make(HashTable)
 
-	json.Unmarshal(b, &indexes)
-	if err != nil {
-		log.Fatalf("FATAL: Could not open index file, %s", err)
-	}
+	json.Unmarshal(data, &indexes)
 	return indexes
 }
 
-func (hf DB) index(key string, end int) {
-	indexes := getIndexes()
+func (hf *DB) index(key string, end int) {
+	indexes := hf.getIndexes()
 	hiv := HashIndexValue{
 		Location: hf.byteSize,
-		Max: hf.byteSize + end,
+		Max:      hf.byteSize + end,
 	}
 	indexes[key] = hiv
 
@@ -81,11 +86,11 @@ func (hf DB) index(key string, end int) {
 		log.Fatalf("FATAL: could not write to file, %s", err)
 	}
 
-	os.WriteFile(INDEX_FILE, fbyte, 0666)
+	os.WriteFile(hf.indexf.Name(), fbyte, 0666)
 }
 
-func (hf DB) Write(key, value string) bool {
-	formattedStr := key + ":" + value + "\n"
+func (hf *DB) Write(key, value string) bool {
+	formattedStr := key + ":" + value
 	nb, err := hf.f.Write([]byte(formattedStr))
 	if err != nil {
 		log.Printf("ERROR: Could not write to file, %s", err)
@@ -97,28 +102,20 @@ func (hf DB) Write(key, value string) bool {
 	return true
 }
 
-func (hf DB) Read(key string) string {
-	db, err := os.Open(DB_FILE)
-	if err != nil {
-		log.Fatalf("FATAL: Unable to open file, err : %s", err)
-	}
-
-	indexes := getIndexes()
+func (hf *DB) Read(key string) string {
+	indexes := hf.getIndexes()
 	hiv := indexes[key]
 
 	size := hiv.Max - hiv.Location
 	data := make([]byte, size)
-	db.ReadAt(data, int64(hiv.Location))
+	hf.f.ReadAt(data, int64(hiv.Location))
 
 	sdata := string(data)
 	splitdata := strings.Split(sdata, ":")
-	return splitdata[len(splitdata) - 1]
+	return splitdata[len(splitdata)-1]
 }
 
-func (hf DB) FlushDB() {
-	os.Remove(INDEX_FILE)
-	log.Println("Remove Index")
-	os.Remove(DB_FILE)
-	log.Println("Remove DB logs")
-	log.Println("FLUSHED ALL")
+func (hf *DB) FlushDB() {
+	hf.f.Truncate(0)
+	hf.indexf.Truncate(0)
 }
